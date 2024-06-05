7fc7894c9928f56284e5efe17314e89338181375
DevikaRajesh22 <devikaraj699@gmail.com>
2024-06-05 18:03:10 +0530

    routes corrected

--- client/src/components/userLoggedIn.jsx
+++ client/src/components/userLoggedIn.jsx
@@ -5,7 +5,7 @@ import { Navigate, Outlet } from "react-router-dom";
 const UserLoggedIn = () => {
     const userInfo = useSelector((state) => state.auth);
     return (
-        userInfo.userInfo ? < Outlet /> : <Navigate to='/users/login' />
+        userInfo.userInfo ? < Outlet /> : <Navigate to='/login' />
     )
 }
 
--- client/src/components/userLoggedOut.jsx
+++ client/src/components/userLoggedOut.jsx
@@ -4,7 +4,7 @@ import { Navigate, Outlet } from "react-router-dom";
 const UserLoggedOut = () => {
     const userInfo = useSelector((state) => state.auth);
     return (
-        userInfo.userInfo ? <Navigate to='/users/topics' /> : <Outlet />
+        userInfo.userInfo ? <Navigate to='/topics' /> : <Outlet />
     )
 }
 
