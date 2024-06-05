89d9a4bacdcbc4698e5b937939aef7528efe95c9
DevikaRajesh22 <devikaraj699@gmail.com>
2024-06-05 16:47:45 +0530

    urls changed

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
     cors({
-        origin: ["https://quizmaster-eight.vercel.app"],
+        origin: ["https://quizmaster-eight.vercel.app/"],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
         optionsSuccessStatus: 200
